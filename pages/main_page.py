13edcf27cc6fc443e9b43bc3ad7b2f4d0fa552cf
Andrey <zhil.andrey2013@yandex.ru>
2022-08-05 21:07:45 +0500

    first_test_PageObject

---
+++ pages/main_page.py
@@ -0,0 +1,8 @@
+from .base_page import BasePage #у кого то с точкой работает перед base_page
+from selenium.webdriver.common.by import By
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()

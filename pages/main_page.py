a0a3794d06d39468c96df4e9b628a225dff7780c
Andrey <zhil.andrey2013@yandex.ru>
2022-08-07 00:27:13 +0500

    Переменная LoginLink

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,23 @@
 from .base_page import BasePage #у кого то с точкой работает перед base_page
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators #у кого то с точкой работает перед loc...
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK)
+        #login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        #login_link.click()
 
-    '''def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")'''
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        #self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+
+
+
+
+
+    """def should_be_login_link(self): #используется в тандеме с файлом локатор там редактируется переменная тут указывается просто переменная
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), Login link is not presented"""
